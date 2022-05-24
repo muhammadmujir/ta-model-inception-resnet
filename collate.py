5909629175e7e3c8d8b12ef0e637317e13acadbb
muhammadmujir <muhammadmujir011@gmail.com>
2022-05-24 15:02:20 +0700

    trial collate

--- collate.py
+++ collate.py
@@ -45,7 +45,9 @@ def default_collate(batch):
 
     elem = batch[0]
     elem_type = type(elem)
+    # print("Batch: ", batch)
     if isinstance(elem, torch.Tensor):
+        # print("tensor ya: ", elem)
         out = None
         if torch.utils.data.get_worker_info() is not None:
             # If we're in a background process, concatenate directly into a
@@ -53,34 +55,45 @@ def default_collate(batch):
             numel = sum(x.numel() for x in batch)
             storage = elem.storage()._new_shared(numel)
             out = elem.new(storage)
-        return torch.stack(batch, 0, out=out)
+        # print("Batch: ", batch[0].shape)
+        # return torch.stack(batch, 0, out=out)
+        return torch.stack(batch, 0)
     elif elem_type.__module__ == 'numpy' and elem_type.__name__ != 'str_' \
             and elem_type.__name__ != 'string_':
         if elem_type.__name__ == 'ndarray' or elem_type.__name__ == 'memmap':
             # array of string classes and object
+            # print("ndarray ya: ", elem)
             if np_str_obj_array_pattern.search(elem.dtype.str) is not None:
                 raise TypeError(default_collate_err_msg_format.format(elem.dtype))
-
+            # print("ndArray: ", elem.shape)
             return default_collate([torch.as_tensor(b) for b in batch])
         elif elem.shape == ():  # scalars
+            # print("scalar ya: ", elem)
             return torch.as_tensor(batch)
     elif isinstance(elem, float):
+        # print("float ya: ", elem)
         return torch.tensor(batch, dtype=torch.float64)
     elif isinstance(elem, int):
+        # print("int ya: ", elem)
         return torch.tensor(batch)
     elif isinstance(elem, string_classes):
+        # print("str ya: ", elem)
         return batch
     elif isinstance(elem, collections.abc.Mapping):
+        # print("map ya: ", elem)
         return {key: default_collate([d[key] for d in batch]) for key in elem}
     elif isinstance(elem, tuple) and hasattr(elem, '_fields'):  # namedtuple
+        # print("tuple ya: ", elem)
         return elem_type(*(default_collate(samples) for samples in zip(*batch)))
     elif isinstance(elem, collections.abc.Sequence):
         # check to make sure that the elements in batch have consistent size
+        # print("bukan tensor ya: ", elem)
         it = iter(batch)
         elem_size = len(next(it))
         if not all(len(elem) == elem_size for elem in it):
             raise RuntimeError('each element in list of batch should be of equal size')
         transposed = zip(*batch)
+        # print("Transposed: ", transposed)
         return [default_collate(samples) for samples in transposed]
 
     raise TypeError(default_collate_err_msg_format.format(elem_type))
