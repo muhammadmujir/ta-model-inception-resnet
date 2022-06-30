a93fe8a07095b5cc5a9b331ff8ad39ecb99a9409
unknown <muhammadmujir011@gmail.com>
2022-06-30 10:54:10 +0700

    testing plot

---
+++ plot.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 30 10:52:11 2022
+
+@author: Mujir
+"""
+import matplotlib.pyplot as plt
+import numpy as np
+
+def main():
+    painting=plt.imread("https://www.freepnglogos.com/uploads/server-png/home-server-icon-icons-and-png-backgrounds-30.png")
+    for i in range(2):
+      plt.figure()
+      plt.imshow(painting)
+      
+if __name__ == '__main__' :
+    main()
