a24740b129bee58894f48e7b507e1b302498a00a
unknown <muhammadmujir011@gmail.com>
2022-06-30 07:46:41 +0700

    create testing model

--- val.py
+++ val.py
@@ -17,7 +17,7 @@ from scipy.ndimage.filters import gaussian_filter
 import scipy
 import json
 import torchvision.transforms.functional as F
-from matplotlib import cm as CM
+from matplotlib import cm
 from image import *
 from model import CSRNet
 import torch
@@ -25,86 +25,155 @@ from torchvision import datasets, transforms
 from tqdm import tqdm
 from inception_restnet_v2.inceptionresnetv2 import InceptionResNetV2
 from constant import *
+import torch
+import torch.nn as nn
+import argparse
+
+parser = argparse.ArgumentParser(description='Model Testing')
+parser.add_argument('img_path', metavar='TEST_IMAGE', help='path to testing image')
+parser.add_argument('gpu',metavar='GPU', type=str, help='GPU id to use.')
+parser.add_argument('best_result_count', metavar='BEST_RESULT_COUNT', help='best result count')
 
 transform=transforms.Compose([
                       transforms.ToTensor(),transforms.Normalize(
                           mean=[0.485, 0.456, 0.406],
                           std=[0.229, 0.224, 0.225]),
                   ])
-isCudaAvailable = True
 
-#defining the location of dataset
-#root = 'C:\\Users\\Admin\\Desktop\\Kuliah\\TA\\ShanghaiTech\\'
-root = BASE_PATH
-# part_A_train = os.path.join(root,'part_A\\train_data','images')
-# part_A_test = os.path.join(root,'part_A\\test_data','images')
-# part_B_train = os.path.join(root,'part_B\\train_data','images')
-# part_B_test = os.path.join(root,'part_B\\test_data','images')
-part_A_train = os.path.join(root,DATASET1_TRAIN_A)
-part_A_test = os.path.join(root,DATASET1_TEST_A)
-part_B_train = os.path.join(root,DATASET1_TRAIN_B)
-part_B_test = os.path.join(root,DATASET1_TEST_B)
-path_sets = [part_A_test]
-
-#defining the image path
-img_paths = []
-for path in path_sets:
-    for img_path in glob.glob(os.path.join(path, '*.jpg')):
-       img_paths.append(img_path)
-
-#model = CSRNet()
-model = InceptionResNetV2()
-
-#defining the model
-if (isCudaAvailable):
-    model = model.cuda()
-else:
-    model = model.cpu()
-#loading the trained weights
-checkpoint = torch.load(CHECKPOINT_PATH+'0model_best.pth.tar')
-model.load_state_dict(checkpoint['state_dict'])
-
-mae = 0
-for i in tqdm(range(len(img_paths))):
-    img = None
+def main():
+    args = parser.parse_args()
+    img_path = args.img_path
+    best_result_count = args.best_result_count
+    isCudaAvailable = True if args.gpu != 'None' else False 
+    maeByCount = 0.0
+    maeByPixel = 0.0
+    bestMaeResult = []
+    bestPixelMaeResult = []
+    pathResult = []
+    bestOutputDensity = []
+    model = CSRNet()
+    maeCriterion = nn.L1Loss(size_average=False).cuda() if isCudaAvailable else nn.L1Loss(size_average=False).cpu()
+    
+    paths = glob.glob(os.path.join(img_path, '*.jpg'))
+    for path in paths:
+        if (isCudaAvailable):
+            img = transform(Image.open(path).convert('RGB')).cuda()
+        else:
+            img = transform(Image.open(path).convert('RGB')).cpu()
+        gt_file = h5py.File(path.replace('.jpg','.h5').replace('images','ground_truth'),'r')
+        groundtruth = np.asarray(gt_file['density'])
+        output = model(img.unsqueeze(0))
+        if (isCudaAvailable):
+            mae = abs(output.detach().cuda().sum().numpy()-np.sum(groundtruth))
+        else:
+            mae = abs(output.detach().cpu().sum().numpy()-np.sum(groundtruth))
+        
+        maeByCount += mae
+        pixelMae = maeCriterion(output, groundtruth).item()
+        maeByPixel += pixelMae
+        
+        if len(bestMaeResult) < best_result_count-1:
+            bestMaeResult.append(mae)
+            bestPixelMaeResult.append(pixelMae)
+            pathResult.append(path)
+            bestOutputDensity.append(output)
+        else:
+            indexOfMaxVal = bestMaeResult.index(max(bestMaeResult)) 
+            if mae < max(bestMaeResult):
+                bestMaeResult[indexOfMaxVal] = mae
+                bestPixelMaeResult[indexOfMaxVal] = pixelMae
+                pathResult[indexOfMaxVal] = path
+                bestOutputDensity[indexOfMaxVal] = output
+    print ("AVG MAE : ",maeByCount/len(paths))
+    print ("AVG MAE BY PIXEL: ", maeByPixel/len(paths))
+    for (i, path) in enumerate(pathResult):
+        print(path)
+        plt.imshow(plt.imread(path))
+        temp = np.asarray(h5py.File(path.replace('.jpg','.h5').replace('images','ground_truth'), 'r')['density'])
+        plt.imshow(temp,cmap = cm.jet)
+        if isCudaAvailable:
+            outputDensity = bestOutputDensity[i].detach().cuda()
+        else:
+            outputDensity = bestOutputDensity[i].detach().cpu()
+        temp = np.asarray(outputDensity.reshape(outputDensity.shape[2], outputDensity.shape[3]))
+        plt.imshow(temp,cmap = cm.jet)
+        plt.show()
+                
+def valManyImages():
+    #defining the location of dataset
+    #root = 'C:\\Users\\Admin\\Desktop\\Kuliah\\TA\\ShanghaiTech\\'
+    root = BASE_PATH
+    # part_A_train = os.path.join(root,'part_A\\train_data','images')
+    # part_A_test = os.path.join(root,'part_A\\test_data','images')
+    # part_B_train = os.path.join(root,'part_B\\train_data','images')
+    # part_B_test = os.path.join(root,'part_B\\test_data','images')
+    part_A_train = os.path.join(root,DATASET1_TRAIN_A)
+    part_A_test = os.path.join(root,DATASET1_TEST_A)
+    part_B_train = os.path.join(root,DATASET1_TRAIN_B)
+    part_B_test = os.path.join(root,DATASET1_TEST_B)
+    path_sets = [part_A_test]
+    
+    #defining the image path
+    img_paths = []
+    for path in path_sets:
+        for img_path in glob.glob(os.path.join(path, '*.jpg')):
+           img_paths.append(img_path)
+    
+    #model = CSRNet()
+    model = InceptionResNetV2()
+    
+    #defining the model
     if (isCudaAvailable):
-        img = transform(Image.open(img_paths[i]).convert('RGB')).cuda()
+        model = model.cuda()
     else:
-        img = transform(Image.open(img_paths[i]).convert('RGB')).cpu()
-    gt_file = h5py.File(img_paths[i].replace('.jpg','.h5').replace('images','ground-truth'),'r')
-    groundtruth = np.asarray(gt_file['density'])
+        model = model.cpu()
+    #loading the trained weights
+    checkpoint = torch.load(CHECKPOINT_PATH+'model_best.pth.tar')
+    model.load_state_dict(checkpoint['state_dict'])
+    
+    mae = 0
+    for i in tqdm(range(len(img_paths))):
+        img = None
+        if (isCudaAvailable):
+            img = transform(Image.open(img_paths[i]).convert('RGB')).cuda()
+        else:
+            img = transform(Image.open(img_paths[i]).convert('RGB')).cpu()
+        gt_file = h5py.File(img_paths[i].replace('.jpg','.h5').replace('images','ground-truth'),'r')
+        groundtruth = np.asarray(gt_file['density'])
+        output = model(img.unsqueeze(0))
+        if (isCudaAvailable):
+            mae += abs(output.detach().cuda().sum().numpy()-np.sum(groundtruth))
+        else:
+            mae += abs(output.detach().cpu().sum().numpy()-np.sum(groundtruth))
+    print ("MAE : ",mae/len(img_paths))
+
+def valSingleImage():
+    # prediction on single image
+    from matplotlib import cm as c
+    img = transform(Image.open('C:\\Users\\Admin\\Desktop\\Kuliah\\TA\\ShanghaiTech\\part_B\\test_data\\images\\IMG_1.jpg').convert('RGB')).cpu()
+    
     output = model(img.unsqueeze(0))
-    if (isCudaAvailable):
-        mae += abs(output.detach().cuda().sum().numpy()-np.sum(groundtruth))
-    else:
-        mae += abs(output.detach().cpu().sum().numpy()-np.sum(groundtruth))
-print ("MAE : ",mae/len(img_paths))
+    print("Predicted Count : ",int(output.detach().cpu().sum().numpy()))
+    temp = np.asarray(output.detach().cpu().reshape(output.detach().cpu().shape[2],output.detach().cpu().shape[3]))
+    plt.imshow(temp,cmap = c.jet)
+    plt.show()
 
+def checkSimilarity():
+    mat = io.loadmat("D:\\TA\Dataset\\ShanghaiTech-Sample\\ShanghaiTech\\part_B\\test_data\\ground-truth\\GT_IMG_281.mat")
+    print("Ground Truth Count", len(mat['image_info'][0][0][0][0][0]))
+    temp = h5py.File('D:\\TA\Dataset\\ShanghaiTech-Sample\\ShanghaiTech\\part_B\\test_data\\ground-truth-h5\\IMG_281.h5', 'r')
+    temp_1 = np.asarray(temp['density'])
+    temp2 = h5py.File('C:\\Users\\Admin\\Desktop\\TA\\Dataset\\ShanghaiTech\\part_B\\test_data\\ground_truth\\IMG_281.h5', 'r')
+    temp_2 = np.asarray(temp2['density'])
+    isSame = temp_1 - temp_2
+    print(len(np.nonzero(isSame)[0]) == 0)
+    plt.imshow(temp_1,cmap = CM.jet)
+    print("After Gaussian Count : ",int(np.sum(temp_1)))
+    print(len(np.nonzero(temp_1)[1]))
+    plt.show()
+    print("Original Image")
+    plt.imshow(plt.imread('C:\\Users\\Admin\\Desktop\\TA\\Dataset\\ShanghaiTech\\part_B\\test_data\\images\\IMG_281.jpg'))
+    plt.show()
 
-# prediction on single image
-# from matplotlib import cm as c
-# img = transform(Image.open('C:\\Users\\Admin\\Desktop\\Kuliah\\TA\\ShanghaiTech\\part_B\\test_data\\images\\IMG_1.jpg').convert('RGB')).cpu()
-
-# output = model(img.unsqueeze(0))
-# print("Predicted Count : ",int(output.detach().cpu().sum().numpy()))
-# temp = np.asarray(output.detach().cpu().reshape(output.detach().cpu().shape[2],output.detach().cpu().shape[3]))
-# plt.imshow(temp,cmap = c.jet)
-# plt.show()
-
-
-
-mat = io.loadmat("D:\\TA\Dataset\\ShanghaiTech-Sample\\ShanghaiTech\\part_B\\test_data\\ground-truth\\GT_IMG_281.mat")
-print("Ground Truth Count", len(mat['image_info'][0][0][0][0][0]))
-temp = h5py.File('D:\\TA\Dataset\\ShanghaiTech-Sample\\ShanghaiTech\\part_B\\test_data\\ground-truth-h5\\IMG_281.h5', 'r')
-temp_1 = np.asarray(temp['density'])
-temp2 = h5py.File('C:\\Users\\Admin\\Desktop\\TA\\Dataset\\ShanghaiTech\\part_B\\test_data\\ground_truth\\IMG_281.h5', 'r')
-temp_2 = np.asarray(temp2['density'])
-isSame = temp_1 - temp_2
-print(len(np.nonzero(isSame)[0]) == 0)
-plt.imshow(temp_1,cmap = CM.jet)
-print("After Gaussian Count : ",int(np.sum(temp_1)))
-print(len(np.nonzero(temp_1)[1]))
-plt.show()
-print("Original Image")
-plt.imshow(plt.imread('C:\\Users\\Admin\\Desktop\\TA\\Dataset\\ShanghaiTech\\part_B\\test_data\\images\\IMG_281.jpg'))
-plt.show()
+if __name__ == '__main__':
+    main() 
